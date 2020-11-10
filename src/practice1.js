4eddc9a16da4a89ae343ac2b9d47be6e2375bd00
Xiao Yu <xiao.susan.yu@gmail.com>
2020-11-10 23:08:25 +0800

    feature: pass tests

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) => {
+  const { data, column } = input
+  return data.map(itemArray => {
+    return column.reduce((prev, curr, index) => {
+      return {
+        ...prev,
+        [curr.name] : itemArray[index]
+      }
+    }, {})
+  })
+}
+
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,13 @@
-const inject
+const inject = (items, sections) => {
+  const { content: content1, index: index1 } = sections[0]
+  const { content: content2, index: index2 } = sections[1]
+  return [
+    ...items.slice(0, index1),
+    content1,
+    ...items.slice(index1, index2),
+    content2,
+    ...items.slice(index2)
+  ]
+}
+
 export { inject };
